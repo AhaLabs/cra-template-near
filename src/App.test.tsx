6ed21f078b6e0c495b43c3cd86c0c12a8d35ebd7
Chad Ostrowski <221614+chadoh@users.noreply.github.com>
2022-01-26 13:19:30 -0500

    add Learn NEAR link

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,15 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test("renders learn NEAR link", () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn NEAR/i);
   expect(linkElement).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import reactLogo from "./react-logo.svg";
+import nearLogo from "./near-logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <div className="columns">
+          <div>
+            <img src={reactLogo} className="App-logo" alt="React" />
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </div>
+          <div>
+            <img src={nearLogo} className="App-logo" alt="logo" />
+            <a
+              className="App-link"
+              href="https://www.near.university"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn NEAR
+            </a>
+          </div>
+        </div>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
