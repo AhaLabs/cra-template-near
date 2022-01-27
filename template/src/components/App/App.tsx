5262b22450cee88cd6aea7b99d7c0f431fe4c92d
Chad Ostrowski <221614+chadoh@users.noreply.github.com>
2022-01-27 16:15:03 -0500

    docs: add more info to landing page

--- template/src/components/App/App.tsx
+++ template/src/components/App/App.tsx
@@ -7,31 +7,58 @@ export function App() {
       <Nav />
       <main className="container">
         <h1>Welcome</h1>
+        <p>Edit files and save to reload. Some interesting files:</p>
         <ul>
           <li>
-            Edit <code>src/components/App.tsx</code> and save to reload.
+            <code>.env</code>
           </li>
           <li>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
+            <code>src/utils/near.ts</code>
           </li>
           <li>
-            <a
-              className="App-link"
-              href="https://www.near.university"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn NEAR
-            </a>
+            <code>src/components/Nav/Nav.tsx</code>
+          </li>
+          <li>
+            <code>src/components/App/App.tsx</code>
           </li>
         </ul>
+        <p>
+          Note that this site does not currently do anything with the contract
+          other than sign someone in. For an example of using the{" "}
+          <code>guest-book.testnet</code> contract, see{" "}
+          <a
+            href="https://github.com/near-examples/guest-book"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            near-examples/guest-book
+          </a>
+          . You may also want to learn more about{" "}
+          <a
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            React
+          </a>
+          ,{" "}
+          <a
+            href="https://create-react-app.dev/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            create-react-app
+          </a>
+          , and{" "}
+          <a
+            href="https://www.near.university"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            NEAR
+          </a>
+          .
+        </p>
       </main>
     </>
   );
